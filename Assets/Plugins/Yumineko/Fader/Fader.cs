c75e13a6ec7fc5bedd63df5c827175f49f451b03
yumineko <lucky.duet@gmail.com>
2019-02-25 00:03:35 +0900

    公開

---
+++ Assets/Plugins/Yumineko/Fader/Fader.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : SingletonMonoBehaviour<Fader> {
+    private Canvas faderCanvas = null;
+    private Image blackImage = null;
+    private IEnumerator fade = null;
+
+    void Reset () {
+        Debug.Log ("Reset");
+        gameObject.GetOrAddComponent<CanvasScaler> ();
+        faderCanvas = gameObject.GetOrAddComponent<Canvas> ();
+        faderCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        faderCanvas.sortingOrder = 9999;
+        gameObject.GetOrAddComponent<GraphicRaycaster> ();
+
+        blackImage = gameObject.GetOrAddComponent<Image> ();
+        blackImage.color = Color.black;
+        blackImage.SetAlpha (0.0f);
+        blackImage.enabled = false;
+    }
+
+    void Start () {
+        Reset ();
+    }
+
+    public void FadeOut (float time) {
+        if (fade == null) {
+            fade = Fade (true, time);
+            StartCoroutine (fade);
+        } else {
+            Debug.LogWarning ("既に別のフェードを実行中です！");
+        }
+    }
+
+    public void FadeIn (float time) {
+        if (fade == null) {
+            fade = Fade (false, time);
+            StartCoroutine (fade);
+        } else {
+            Debug.LogWarning ("既に別のフェードを実行中です！");
+        }
+    }
+
+    public IEnumerator Fade (bool fadeout, float time) {
+        float alpha = fadeout ? 0.0f : 1.0f;
+        float countTime = 0.0f;
+        blackImage.enabled = true;
+        blackImage.SetAlpha (alpha);
+        yield return new WaitForEndOfFrame ();
+
+        while (countTime < time) {
+            countTime += Time.deltaTime;
+            alpha = fadeout ? countTime / time : 1.0f - countTime / time;
+            blackImage.SetAlpha (alpha);
+            yield return new WaitForEndOfFrame ();
+        }
+        alpha = fadeout ? 1.0f : 0.0f;
+        blackImage.SetAlpha (alpha);
+        fade = null;
+        blackImage.enabled = fadeout;
+        yield return null;
+    }
+}
---
+++ Assets/Plugins/Yumineko/Scene Loader/LoadScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour {
+    public float FadeOutTime { get; set; }
+    public float FadeInTime { get; set; }
+    public void FadeLoad (string sceneName) {
+        if (FadeOutTime == 0.0f) FadeOutTime = 1.0f;
+        if (FadeInTime == 0.0f) FadeInTime = 1.0f;
+        SceneLoadManager.Instance.FadeLoad (sceneName, FadeOutTime, FadeInTime);
+    }
+
+    public void Load (string sceneName) {
+        SceneLoadManager.Instance.Load (sceneName);
+    }
+}
