9bf44fb2723fbfa7300630abaa8856447043c125
yumineko <lucky.duet@gmail.com>
2019-08-04 04:27:06 +0900

    UniTaskに変更

--- Assets/Plugins/Yumineko/Fader/Fader.cs
+++ Assets/Plugins/Yumineko/Fader/Fader.cs
@@ -1,15 +1,15 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using UniRx.Async;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Fader : SingletonMonoBehaviour<Fader> {
     private Canvas faderCanvas = null;
     private Image blackImage = null;
-    private IEnumerator fade = null;
+    private UniTask fade = default;
 
     void Reset () {
-        Debug.Log ("Reset");
+        gameObject.name = "Fader";
         gameObject.GetOrAddComponent<CanvasScaler> ();
         faderCanvas = gameObject.GetOrAddComponent<Canvas> ();
         faderCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
@@ -27,40 +27,37 @@ public class Fader : SingletonMonoBehaviour<Fader> {
     }
 
     public void FadeOut (float time) {
-        if (fade == null) {
+        Debug.Log("");
+        if (fade.IsCompleted) {
             fade = Fade (true, time);
-            StartCoroutine (fade);
         } else {
             Debug.LogWarning ("既に別のフェードを実行中です！");
         }
     }
 
     public void FadeIn (float time) {
-        if (fade == null) {
+        if (fade.IsCompleted) {
             fade = Fade (false, time);
-            StartCoroutine (fade);
         } else {
             Debug.LogWarning ("既に別のフェードを実行中です！");
         }
     }
 
-    public IEnumerator Fade (bool fadeout, float time) {
+    public async UniTask Fade(bool fadeout, float time){
         float alpha = fadeout ? 0.0f : 1.0f;
         float countTime = 0.0f;
         blackImage.enabled = true;
         blackImage.SetAlpha (alpha);
-        yield return new WaitForEndOfFrame ();
+        await UniTask.DelayFrame(1);
 
         while (countTime < time) {
             countTime += Time.deltaTime;
             alpha = fadeout ? countTime / time : 1.0f - countTime / time;
             blackImage.SetAlpha (alpha);
-            yield return new WaitForEndOfFrame ();
+            await UniTask.DelayFrame(1);
         }
         alpha = fadeout ? 1.0f : 0.0f;
         blackImage.SetAlpha (alpha);
-        fade = null;
         blackImage.enabled = fadeout;
-        yield return null;
     }
 }
--- Assets/Plugins/Yumineko/Scene Loader/SceneLoadManager.cs
+++ Assets/Plugins/Yumineko/Scene Loader/SceneLoadManager.cs
@@ -1,91 +1,52 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using UniRx.Async;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoadManager : SingletonMonoBehaviour<SceneLoadManager> {
-    IEnumerator load = null;
+    UniTask load = default;
     void Reset () {
         if (GameObject.FindObjectOfType<Fader> () == null) {
             gameObject.AddComponent<Fader> ();
         }
-        gameObject.name = "SceneLoader";
+        gameObject.name = "Scene Loader";
     }
 
     public void Load (string sceneName) {
-        if (load == null) {
+        if (load.IsCompleted) {
             load = LoadAsync (sceneName);
-            StartCoroutine (load);
         } else {
             Debug.LogWarning ("既にシーンをロード中です！");
         }
     }
-    IEnumerator LoadAsync (string sceneName) {
+    async UniTask LoadAsync (string sceneName) {
         string unloadSceneName = SceneManager.GetActiveScene ().name;
-        AsyncOperation async;
-        yield return null;
-        async = SceneManager.LoadSceneAsync (sceneName, LoadSceneMode.Additive);
-        while (async.progress < 1.0f) {
-            yield return new WaitForEndOfFrame ();
-        }
-        async = SceneManager.UnloadSceneAsync (unloadSceneName);
-        while (async.progress < 1.0f) {
-            yield return new WaitForEndOfFrame ();
-        }
-        load = null;
-        yield return null;
+        await SceneManager.LoadSceneAsync (sceneName, LoadSceneMode.Additive);
+        await SceneManager.UnloadSceneAsync (unloadSceneName);
     }
 
     public void FadeLoad (string sceneName, float outTime = 1.0f, float inTime = 1.0f) {
-        if (load == null) {
+        if (load.IsCompleted) {
             load = FadeLoadAsync (sceneName, outTime, inTime);
-            StartCoroutine (load);
         } else {
             Debug.LogWarning ("既にシーンをロード中です！");
         }
     }
 
-    IEnumerator FadeLoadAsync (string sceneName, float outTime = 1.0f, float inTime = 1.0f) {
+    async UniTask FadeLoadAsync (string sceneName, float outTime = 1.0f, float inTime = 1.0f) {
         string unloadSceneName = SceneManager.GetActiveScene ().name;
-        AsyncOperation async = default;
-        IEnumerator fade = Fader.Instance.Fade (fadeout: true, time: outTime);
-        yield return null;
+        var f = Fader.Instance;
+        await UniTask.DelayFrame (1);
 
-        //  シーンを非同期で読み込み（切り替えない）
-        async = SceneManager.LoadSceneAsync (sceneName, LoadSceneMode.Additive);
-        async.allowSceneActivation = false;
+        var loadTask = SceneManager.LoadSceneAsync (sceneName, LoadSceneMode.Additive);
+        loadTask.allowSceneActivation = false;
 
-        //  フェードアウト開始
-        StartCoroutine (fade);
+        await Fader.Instance.Fade (fadeout: true, time: outTime);;
 
-        //  フェードアウトが完了し、シーンの読み込みが終わるまで待機
-        while (fade.Current != null || async.progress < 0.9f) {
-            yield return new WaitForEndOfFrame ();
-        }
+        loadTask.allowSceneActivation = true;
+        await loadTask;
 
-        //  シーンの切り替えを許可
-        async.allowSceneActivation = true;
-        yield return new WaitForEndOfFrame ();
-
-        //  シーンの切り替えが終わるまで待機
-        while (async.progress < 1.0f) {
-            yield return new WaitForEndOfFrame ();
-        }
-
-        async = SceneManager.UnloadSceneAsync (unloadSceneName);
-        while (async.progress < 1.0f) {
-            yield return new WaitForEndOfFrame ();
-        }
-
-        //  フェードイン開始
-        fade = Fader.Instance.Fade (fadeout: false, time: inTime);
-        StartCoroutine (fade);
-        while (fade.Current != null) {
-            yield return new WaitForEndOfFrame ();
-        }
-
-        load = null;
-        yield return null;
+        await SceneManager.UnloadSceneAsync (unloadSceneName);
+        await Fader.Instance.Fade (fadeout: false, time: inTime);
     }
 }
